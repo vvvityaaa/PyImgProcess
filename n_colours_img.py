169ef77bace12de27657cc6a02ad34cf48bdf837
Aitur <artur.kunz@rub.de>
2017-09-21 11:41:19 +0200

    Update n_colours_img.py

--- n_colours_img.py
+++ n_colours_img.py
@@ -8,26 +8,32 @@ param image_2_check: image that should be checked
 return: should return a boolean; True means it is a grey picture and false means it is a coloured pictures
 '''
 
+
 def check_if_image_grey(image_2_check):
-    return len(image_2_check[0:0:]) == 1 #If the picture is grey, than the third dimension should have the length 1
+    return np.ndim(image_2_check)  # If the picture is grey, than the third dimension should have the length 1
+
 
 def n_colours_img(path, colours):
-
     '''
     Function should convert grey_value_image to n-coloured image.
     :param path: path to the image, colours: array with colours
     :return: should return or just plot resulting image.
     '''
 
-    i = Image.open(path)
-    if(check_if_grey(i) == True):
-        max_Value = np.max(i)
-        min_Value = np.min(i)
+    image_2_change = Image.open(path)
+    array = np.asanyarray(image_2_change, dtype=np.float32)
+    if check_if_image_grey(image_2_change):
+        max_value = np.max(array)
+        min_value = np.min(array)
         step_count = len(colours)
-        step_width = (max_value-min_Value)/step_count
-        for step in range(max_vaule, min_value, step_count):
-            for 
-            if(((i - step)<step_width) & ((i-step)>0))):
-                
+        step_width = (max_value - min_value) / step_count
+        for step in np.arange(max_value, min_value, step_count):
+            changed_image = [step for pixel in array if ((pixel - step) < step_width) & ((pixel - step) > 0)]
+        return changed_image
     else:
         print("The picture should be grey")
+
+changed = n_colours_img("C:\\Users\\Artur\\Pictures\\lena.png", [100, 200, 345])
+print(changed)
+#plt.imshow(changed, cmap='Greys', interpolation='nearest')
+#plt.show()
