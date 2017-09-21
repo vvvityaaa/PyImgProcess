e5e180a6c3e096302148a6dc040dd5c7eed1f321
Viktor Studenyak <viktor.studenyak@ruhr-uni-bochum.de>
2017-09-21 13:16:28 +0200

    Updated prozent_of_image, changed function name

--- color_proportion.py
+++ color_proportion.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def prozent_of_img(path):
+def color_proportion(path):
     """
     The function creates a piechart that shows the portions of red, green and blue in the picture
     :param path: path to the picture
@@ -18,19 +18,20 @@ def prozent_of_img(path):
         value_list.append(sum(sum(array[:, :, 2])))
         return value_list
     except TypeError:
-        return "The params seems not to be rigth"
+        return "Parameters seem not to be right"
     except OSError:
         return "This isn't an image"
     except IndexError:
         return "The image isn't colored"
+
+
 if __name__ == "__main__":
-    piechart = portionsofcolours('lena.jpg')
+    piechart = color_proportion('lena.jpg')
     if type(piechart) != str:
-        explode = (0,0,0)
+        explode = (0, 0, 0)
         labels = ['Red', 'Green', 'Blue']
         colors = ['red', 'green', 'blue']
-        plt.pie(piechart, explode=explode, labels=labels, colors=colors,
-            autopct='%1.1f%%', shadow=True, startangle=140)
+        plt.pie(piechart, explode=explode, labels=labels, colors=colors, autopct='%1.1f%%', shadow=True, startangle=140)
         plt.ylabel("Farbanteil")
         plt.show()
     else:
