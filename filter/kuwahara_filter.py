edb5ac908943642e401c4625fff1da65ec703517
Viktor Studenyak <viktor.studenyak@ruhr-uni-bochum.de>
2017-09-22 10:31:10 +0200

    Implemented median_filter

--- filter/kuwahara_filter.py
+++ filter/kuwahara_filter.py
@@ -7,27 +7,30 @@ import math
 def kuwahara_filter(path, mask_size):
 
     '''
-    Applies kuwahara filter on the given image (Folien 4 Seite 44)
+    Applies kuwahara filter on the given image
     :param path:
     :param mask_size: should be odd number, at least 3
     :return: filtered image
     '''
+
+    # variable initialization
     i = Image.open(path)
     image_array = np.asarray(i, dtype=int)
     image_height, image_width = i.size
     half_size = math.floor(mask_size/2)
+    resulting_img = np.zeros(image_height * image_width).reshape(image_height, image_width)
 
     for i in range(half_size, image_height-half_size):
 
         for j in range(half_size, image_width-half_size):
 
-            #region extraction
-            top_left = (image_array[i-half_size:i+1, j-half_size:j+1]).ravel() # extracts every region from array
-            top_right = (image_array[i-half_size:i+1, j:j+half_size+1]).ravel() # and converts it into 1D array
+            # region extraction
+            top_left = (image_array[i-half_size:i+1, j-half_size:j+1]).ravel()  # extracts every region from array
+            top_right = (image_array[i-half_size:i+1, j:j+half_size+1]).ravel()  # and converts it into 1D array
             bottom_left = (image_array[i:i+half_size+1, j-half_size:j+1]).ravel()
             bottom_right = (image_array[i:i+half_size+1, j:j+half_size+1]).ravel()
 
-            #variance calculation
+            # variance calculation
             tl_var = top_left.var()
             tr_var = top_right.var()
             bl_var = bottom_left.var()
@@ -38,15 +41,15 @@ def kuwahara_filter(path, mask_size):
 
             # finds out which region has the lowest variance, current pixel gets than the mean of that region
             if m == tl_var:
-                image_array[i,j] = np.mean(top_left)
+                resulting_img[i, j] = np.mean(top_left)
             elif m == tr_var:
-                image_array[i, j] = np.mean(top_right)
+                resulting_img[i, j] = np.mean(top_right)
             elif m == bl_var:
-                image_array[i, j] = np.mean(bottom_left)
+                resulting_img[i, j] = np.mean(bottom_left)
             elif m == br_var:
-                image_array[i, j] = np.mean(bottom_right)
+                resulting_img[i, j] = np.mean(bottom_right)
 
-    return image_array
+    return resulting_img
 
 if __name__ == "__main__":
     kuwahara_img = kuwahara_filter('lena.png', 5)
--- filter/median_filter.py
+++ filter/median_filter.py
@@ -1,16 +1,36 @@
 from PIL import Image
 import numpy as np
 import matplotlib.pyplot as plt
+import math
 
 
-def median_filter(path, regionSize):
+def median_filter(path, region_size):
+
     '''
-    Values for every pixel equals to the median of all values in the region (Folien 4 Seite 36)
-    :param path:
-    :param regionSize:
-    :return:
+    Values for every pixel equals to the median of all values in the region
+    :param path: path to the image
+    :param region_size: size of the region that will be extracted and its median will be calculated
+    :return: resulting image
     '''
 
+    # initializing image and other helping variables
+    i = Image.open(path)
+    image_array = np.asarray(i, dtype=int)
+    image_height, image_width = i.size
+    half_size = math.floor(region_size/2)
+    resulting_img = np.zeros(image_height * image_width).reshape(image_height, image_width)
+    # iterating over the image
+    for i in range(half_size, image_height-half_size):
+
+        for j in range(half_size, image_width-half_size):
+            # extraction of region of the given size and calculation of its median
+            region = (image_array[i-half_size:i+half_size+1, j-half_size:j+half_size+1]).ravel()
+            resulting_img[i, j] = np.median(region)
+
+    return resulting_img
 
 
 if __name__ == "__main__":
+    median_img = median_filter('lena.png', 3)
+    plt.imshow(median_img, cmap='gray', interpolation='nearest')
+    plt.show()
