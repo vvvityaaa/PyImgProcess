59f7762fc4d99b8f7e3cf75463897e69f701de84
Viktor Studenyak <viktor.studenyak@ruhr-uni-bochum.de>
2017-09-22 09:56:16 +0200

    Improved linear_filter

--- filter/kuwahara_filter.py
+++ filter/kuwahara_filter.py
@@ -20,18 +20,22 @@ def kuwahara_filter(path, mask_size):
     for i in range(half_size, image_height-half_size):
 
         for j in range(half_size, image_width-half_size):
+
             #region extraction
             top_left = (image_array[i-half_size:i+1, j-half_size:j+1]).ravel() # extracts every region from array
             top_right = (image_array[i-half_size:i+1, j:j+half_size+1]).ravel() # and converts it into 1D array
             bottom_left = (image_array[i:i+half_size+1, j-half_size:j+1]).ravel()
             bottom_right = (image_array[i:i+half_size+1, j:j+half_size+1]).ravel()
+
             #variance calculation
             tl_var = top_left.var()
             tr_var = top_right.var()
             bl_var = bottom_left.var()
             br_var = bottom_right.var()
+
             # calculates the min of variances of all regions
             m = np.amin([tl_var, tr_var, bl_var, br_var])
+
             # finds out which region has the lowest variance, current pixel gets than the mean of that region
             if m == tl_var:
                 image_array[i,j] = np.mean(top_left)
@@ -41,6 +45,7 @@ def kuwahara_filter(path, mask_size):
                 image_array[i, j] = np.mean(bottom_left)
             elif m == br_var:
                 image_array[i, j] = np.mean(bottom_right)
+
     return image_array
 
 if __name__ == "__main__":
--- filter/linear_filter.py
+++ filter/linear_filter.py
@@ -5,12 +5,14 @@ import math
 
 
 def linear_filter(path, mask):
-    """
+
+    '''
     Filters the image on the given path with linear filter mask. (Folien 3 Seite 46 und Folien 4 Seite 27)
     :param path:
     :param mask:
     :return: filtered image
-    """
+    '''
+
     try:
         image = Image.open(path)
         img_array = np.asarray(image, dtype=np.float32)
@@ -18,75 +20,34 @@ def linear_filter(path, mask):
         return "Parameters seem not to be right"
     except OSError:
         return "This isn't an image"
-    sizeX, sizeY = img_array.shape
+    size_x, size_y = img_array.shape
     height_m, width_m = mask.shape
-    border_height = int(height_m/2)
-    border_width = int(width_m/2)
-    result = np.zeros(sizeX*sizeY).reshape(sizeX, sizeY)
+    border_height = math.floor(height_m/2)
+    border_width = math.floor(width_m/2)
+    result = np.zeros(size_x*size_y).reshape(size_x, size_y)
 
-    #The factor for normalisation is set
+    # The factor for normalisation is set
     factor = sum(sum(mask))
 
-    for i in range(1+border_height, sizeX-border_height):
-        for j in range(1+border_width, sizeY-border_width):
-            value = 0.0
-            y = 1
+    for i in range(border_height, size_x-border_height+1):
+        for j in range(border_width, size_y-border_width+1):
+            value = 0
+            x = 0  # Variable to iterate over a mask
             for k in range(i-border_height, i+border_height):
-                x = 1
-                for m in range(j-border_width,j+border_width):
-                    #the real calculation; multiplication with the mask
-                    value = value + img_array[k, m]*mask[y, x]
+                y = 0  # Variable to iterate over a mask
+                for m in range(j-border_width, j+border_width):
+                    # the real calculation; multiplication with the mask
+                    value = value + img_array[k, m]*mask[x, y]
+                    y += 1
+                x += 1
             if factor != 0:
                 value = value/factor
             result[i, j] = value
     return result
 
-"""
-SAMPLE CODE FrOM MATLAB:
-function result = ImageFiltering(image,mask)
-[sizeX, sizeY] = size(image);
-[heightM, widthM] = size(mask);
-borderHeight = floor(heightM/2);
-borderWidth = floor(widthM/2);
-factor =  0;
-result = zeros(sizeX,sizeY);
-%Hier wird die Normalisierung durchgeführt
-for i=1:heightM
-    for j=1:widthM
-        factor = factor + mask(i,j);
-    end
-end
-%4 Schleifen, die ersten beiden gehen durch das Bild durch
-%die letzen beiden durch die Maske
-for i = (1+borderHeight):(sizeX-borderHeight)
-    for j = (1+borderWidth):(sizeY-borderWidth)
-        value = 0.0;
-        y = 1;
-        for k = (i-borderHeight):(i+borderHeight)
-            x = 1;
-            for m = (j-borderWidth):(j+borderWidth)
-                %die eigentliche Berechnung, Multiplikation des Pixelwertes
-                %mit der Maske
-                value = value + double(image(k,m)*mask(y,x));
-                x = x + 1;
-            end
-            y = y + 1;
-        end
-        if factor~=0
-            value = value/factor;
-        end
-        result(i,j) = value;
-    end
-end
-%image muss von den double zu den uint8 Format wechseln, um das Bild
-%richtig darstellen zu können
-result = uint8(result);
-end
 
-
-if __name__ == "__main__":"""
 if __name__ == "__main__":
-    result_img = linear_filter('lena.png', np.arange(9).reshape(3, 3))
+    result_img = linear_filter('lena.png', np.arange(49).reshape(7, 7))
     if type(result_img) != str:
         plt.imshow(result_img, cmap='gray', interpolation='nearest')
         plt.show()
